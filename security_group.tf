ab5a68aec74de1830a86d66aa93d528bd355d796
Sonali2024 <92144576+Sonali2024@users.noreply.github.com>
2021-10-11 10:31:15 +0530

    update security.tf

--- security_group.tf
+++ security_group.tf
@@ -1,26 +1,26 @@
 resource "aws_security_group" "prod-SG" {
   name        = "${var.project_name}-${var.environment}-SG"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.production.id
+  vpc_id      = aws_vpc.staging.id
 
   ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
-    cidr_blocks = [ aws_vpc.production.cidr_block ]
+    cidr_blocks = [ aws_vpc.staging.cidr_block ]
 }
 
 ingress {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = [ aws_vpc.production.cidr_block ]
+    cidr_blocks = [ aws_vpc.staging.cidr_block ]
 }
 
 ingress {
     from_port = 53
     to_port = 53
     protocol = "tcp"
-    cidr_blocks = [ aws_vpc.production.cidr_block ]
+    cidr_blocks = [ aws_vpc.staging.cidr_block ]
 }
 }
